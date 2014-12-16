8a965dc9f48c85604f449f9640b46c112a2b76a5
Luke Branch <lukebranch@gmail.com>
2014-12-16 17:37:49 +0800

    Update view_list.js

--- static/src/js/view_list.js
+++ static/src/js/view_list.js
@@ -7,12 +7,12 @@ openerp.nfx_product_image = function(openerp) {
             var value = row_data[this.id].value;
             var download_url;
             if (value && value.substr(0, 10).indexOf(' ') == -1) {
-                download_url = "data:application/octet-stream;base64," + value;
+                download_url = "data:image/png;base64," + value;
             } else {
                 download_url = this.placeholder;
             }
 
-            return _.template("<img widget='image' width='50' height='50' src='<%-href%>'/>", {
+            return _.template('<img width="50" height="50" src="<%-href%>"/>', {
                 href: download_url,
             });
         }
